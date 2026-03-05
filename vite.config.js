fb2fd867645cbcc218223642b88cc363c869a52e
MohammedShafiyaKhatun <shafushafiyakhatun@gmail.com>
2026-03-05 20:42:47 +0530

    updatedrepo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- base: "/CARTAPP/",
+ base: "/MYCARTAPP/",
   plugins: [tailwindcss(), react()],
 });
